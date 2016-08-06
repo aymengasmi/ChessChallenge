c6e48c224b48e90c57d7731fc88be7703990a02a
aymgasmi <aymguesmi@gmail.com>
2016-08-07 00:58:51 +0200

    Add attack function to KING class

--- app/chess/piece.py
+++ app/chess/piece.py
@@ -1,49 +1,64 @@
+from math import hypot
+
+
 class ChessPiece(object):
 
     def __init__(self):
-        self.column = 0
-        self.row = 0
+        self.x = 0
+        self.y = 0
         self.symbol = ''
 
     # Checks piece can attack the specified position
-    def can_attack_position(self, column, row):
-        pass
+    def deplace_piece(self, square):
+        self.x = square.x
+        self.y = square.y
 
     # return the character representation of this chess piece
     def get_symbol(self):
         return self.symbol
 
     def set_column(self, column):
-        self.column = column
+        self.x = column
 
     def get_column(self):
-        return self.column
+        return self.x
 
     def set_row(self, row):
-        self.row = row
+        self.y = row
 
     def get_row(self):
-        return self.row
+        return self.y
+
+    def pos(self):
+        return(self.x, self.y)
+
+    def check_attack(self, p):
+        return None
 
 
 class King(ChessPiece):
 
     def __init__(self):
+        ChessPiece.__init__(self)
         self.symbol = 'K'
 
         print '>>> Buil king piece'
 
-    def can_attack_position(self, column, row):
-        return True
+    def check_attack(self, pos):
+        dist = hypot(self.x - pos.x, self.y - pos.y)
+        if dist <= 1:
+            return True
+        else:
+            return False
 
 
 class Queen(ChessPiece):
 
     def __init__(self):
+        ChessPiece.__init__(self)
         self.symbol = 'Q'
 
         print '>>> Buil Queen piece'
 
-    def can_attack_position(self, column, row):
+    def check_attack(self, pos):
         return True
-
