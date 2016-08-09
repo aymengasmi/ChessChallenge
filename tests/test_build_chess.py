2b9e3ad0f588cc9e1efdbdf35fd9bec95b30da2e
Aymen Gasmi <gasmi.aym@gmail.com>
2016-08-09 18:23:24 +0200

    Add tests methods (only one type on the board)

--- tests/test_build_chess.py
+++ tests/test_build_chess.py
@@ -22,10 +22,10 @@ class TestBuildChess(unittest.TestCase):
     # ///////////////////////////////////////////////////
     def test_build_chess(self):
         """Tests validity of build chessboard"""
+
         self.chess.pieces_types.sort()
         self.assertEqual(self.chess.pieces_types == ['B','K', 'K','N', 'Q','R'], True)
         self.assertEqual(self.chess.number_pieces == 6, True)
-    
     
     def test_solution_only_kings(self):
         params = [5, 5]
@@ -42,6 +42,32 @@ class TestBuildChess(unittest.TestCase):
         self.chess = Chess(params)
         self.chess.run_game()
         self.assertEqual(self.chess.solutions == 140, True)
+                
+    def test_solution_only_bishops(self):
+        params = [5, 5]
+        pieces = {'King': 0, 'Queen': 0, 'Bishop': 2, 'Rook': 0, 'Knight': 0}
+        params.append(pieces)
+        self.chess = Chess(params)
+        self.chess.run_game()
+        self.assertEqual(self.chess.solutions == 240, True)
+                
+    def test_solution_only_rooks(self):
+        params = [5, 5]
+        pieces = {'King': 0, 'Queen': 0, 'Bishop': 0, 'Rook': 2, 'Knight': 0}
+        params.append(pieces)
+        self.chess = Chess(params)
+        self.chess.run_game()
+        self.assertEqual(self.chess.solutions == 200, True)
+                
+    def test_solution_only_knights(self):
+        params = [5, 5]
+        pieces = {'King': 0, 'Queen': 0, 'Bishop': 0, 'Rook': 0, 'Knight': 2}
+        params.append(pieces)
+        self.chess = Chess(params)
+        self.chess.run_game()
+        self.assertEqual(self.chess.solutions == 252, True)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
