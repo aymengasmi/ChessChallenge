0e6c6aa2f47ca65bd9b2f33dbf2a5a659e906093
Aymen Gasmi <gasmi.aym@gmail.com>
2016-08-10 13:41:49 +0200

    Update input parameters

--- app/run.py
+++ app/run.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import time
 import sys
 from chess.chess import Chess
-
 
 if __name__ == "__main__":
     if len(sys.argv) >= 3:
         params = sys.argv[1]
     else:
-        params = [3, 3]
+        params = [7, 7]
 
-    pieces = {'King': 2, 'Queen': 0, 'Bishop': 0, 'Rook': 0, 'Knight': 0}
+    pieces = {'King': 2, 'Queen': 2, 'Bishop': 2, 'Rook': 0, 'Knight': 1}
     params.append(pieces)
 
     start = time.time()
@@ -19,5 +20,5 @@ if __name__ == "__main__":
     chess.run_game()
     end = time.time()
     solutions = chess.solutions
-    print "Number of solutions: " + str(solutions)
+    print "Number of solutions: " + str(solutions) + "\n"
     print "Time (sec): " + str(end - start)
